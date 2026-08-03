5e32f3b16496264d36a541f604f38339c3af1344
Abhishek <akabhishek2509@gmail.com>
2026-08-03 19:40:06 +0530

    Improvement

---
+++ BASIC/sum_Digit.java
@@ -0,0 +1,12 @@
+public class sum_Digit {
+    public static void main(String[] args) {
+        int n = 1234;
+        int sum=0;
+        while(n>0){
+            int digit = n %10;
+            sum = sum+digit;
+            n = n/10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+}
